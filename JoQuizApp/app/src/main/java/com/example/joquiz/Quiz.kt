27795d38adb49fbc93edeeb7213e42224c0886ce
Bifmann <98736496+Bifmann@users.noreply.github.com>
2024-02-22 16:55:11 +0100

    Update Journal_de_travail_VMW.xlsx Ajout de la classe Quiz

---
+++ JoQuizApp/app/src/main/java/com/example/joquiz/Quiz.kt
@@ -0,0 +1,11 @@
+package com.example.joquiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Quiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quiz)
+    }
+}
